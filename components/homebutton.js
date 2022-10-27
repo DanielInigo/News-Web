4b4a0299cfcd948fbdf30b2cfd3ca769887551ba
SivaGuhan <sivasguhan@gmail.com>
2022-10-27 15:14:30 +0530

    add button style

--- components/homebutton.js
+++ components/homebutton.js
@@ -6,7 +6,7 @@ export default function homeButton(props){
     return(
         <div
             onClick={() => router.push(temp)}
-            className="w-[50%] font-bold bg-red-700 rounded-full p-3 text-white cursor-pointer text-center"
+            className="but w-[50%] font-bold bg-red-700 rounded-full p-3 text-white cursor-pointer text-center"
           >
             Display News Feed
           </div>
